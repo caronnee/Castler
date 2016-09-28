365f70dd0fdb716a362368732f23bf308f54492b
Eva Peskova <caronnee@gmail.com>
2016-09-28 23:18:09 +0200

    improved main

--- ImplTest/main.cpp
+++ ImplTest/main.cpp
@@ -1,84 +1,94 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/opencv.hpp"
 
-//#define NAME "c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\c2\\0062.jpg"
-//#define NAME "c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\calibrationImages\\Image10.tif"
-//#define NAME "c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\Canon\\IMG_0254.JPG"
-//#define NAME "c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\Canon\\IMG_0257.JPG"
+//#define NAME "calibration\\c2\\0062.jpg"
+//#define NAME "calibration\\calibrationImages\\Image10.tif"
+//#define NAME "calibration\\Canon\\IMG_0254.JPG"
+//#define NAME "calibration\\Canon\\IMG_0257.JPG"
 #define NAME "c:\\work\\DP1\\Castler\\Creator\\calibration\\mobil\\frames\\chess_8_8\\0017a.jpg"
 
 char * names[] = {
-	"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0001.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0002.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0003.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0004.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0005.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0006.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0007.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0008.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0009.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0010.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0011.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0012.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0013.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0014.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0015.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0016.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0017.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0017b.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0018.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0019.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0020.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0021.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0022.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0023.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0024.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0025.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0026.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0027.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0028.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0029.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0030.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0031.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0032.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0033.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0034.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0035.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0036.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0037.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0038.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0039.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0040.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0041.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0042.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0043.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0044.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0045.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0046.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0047.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0048.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0049.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0050.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0051.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0052.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0053.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0054.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0055.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0056.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0057.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0058.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\0059.jpg"
-	,"c:\\work\\DP1\\Castler\\Creator\\bin\\calibration\\mobil\\frames\\chess_8_8\\test.jpg"
-};
+	"calibration\\mobil\\frames\\scaled\\0001.jpg",
+	"calibration\\mobil\\frames\\scaled\\0002.jpg",
+	"calibration\\mobil\\frames\\scaled\\0003.jpg",
+	"calibration\\mobil\\frames\\scaled\\0004.jpg",
+	"calibration\\mobil\\frames\\scaled\\0005.jpg",
+	"calibration\\mobil\\frames\\scaled\\0006.jpg",
+	"calibration\\mobil\\frames\\scaled\\0007.jpg",
+	"calibration\\mobil\\frames\\scaled\\0008.jpg",
+	"calibration\\mobil\\frames\\scaled\\0009.jpg",
+	"calibration\\mobil\\frames\\scaled\\0010.jpg",
+	"calibration\\mobil\\frames\\scaled\\0011.jpg",
+	"calibration\\mobil\\frames\\scaled\\0012.jpg",
+	"calibration\\mobil\\frames\\scaled\\0013.jpg",
+	"calibration\\mobil\\frames\\scaled\\0014.jpg",
+	"calibration\\mobil\\frames\\scaled\\0015.jpg",
+	"calibration\\mobil\\frames\\scaled\\0016.jpg",
+	"calibration\\mobil\\frames\\scaled\\0017.jpg",
+	"calibration\\mobil\\frames\\scaled\\0017b.jpg",
+	"calibration\\mobil\\frames\\scaled\\0018.jpg",
+	"calibration\\mobil\\frames\\scaled\\0019.jpg",
+	"calibration\\mobil\\frames\\scaled\\0020.jpg",
+	"calibration\\mobil\\frames\\scaled\\0021.jpg",
+	"calibration\\mobil\\frames\\scaled\\0022.jpg",
+	"calibration\\mobil\\frames\\scaled\\0023.jpg",
+	"calibration\\mobil\\frames\\scaled\\0024.jpg",
+	"calibration\\mobil\\frames\\scaled\\0025.jpg",
+	"calibration\\mobil\\frames\\scaled\\0026.jpg",
+	"calibration\\mobil\\frames\\scaled\\0027.jpg",
+	"calibration\\mobil\\frames\\scaled\\0028.jpg",
+	"calibration\\mobil\\frames\\scaled\\0029.jpg",
+	"calibration\\mobil\\frames\\scaled\\0030.jpg",
+	"calibration\\mobil\\frames\\scaled\\0031.jpg",
+	"calibration\\mobil\\frames\\scaled\\0032.jpg",
+	"calibration\\mobil\\frames\\scaled\\0033.jpg",
+	"calibration\\mobil\\frames\\scaled\\0034.jpg",
+	"calibration\\mobil\\frames\\scaled\\0035.jpg",
+	"calibration\\mobil\\frames\\scaled\\0036.jpg",
+	"calibration\\mobil\\frames\\scaled\\0037.jpg",
+	"calibration\\mobil\\frames\\scaled\\0038.jpg",
+	"calibration\\mobil\\frames\\scaled\\0039.jpg",
+	"calibration\\mobil\\frames\\scaled\\0040.jpg",
+	"calibration\\mobil\\frames\\scaled\\0041.jpg",
+	"calibration\\mobil\\frames\\scaled\\0042.jpg",
+	"calibration\\mobil\\frames\\scaled\\0043.jpg",
+	"calibration\\mobil\\frames\\scaled\\0044.jpg",
+	"calibration\\mobil\\frames\\scaled\\0045.jpg",
+	"calibration\\mobil\\frames\\scaled\\0046.jpg",
+	"calibration\\mobil\\frames\\scaled\\0047.jpg",
+	"calibration\\mobil\\frames\\scaled\\0048.jpg",
+	"calibration\\mobil\\frames\\scaled\\0049.jpg",
+	"calibration\\mobil\\frames\\scaled\\0050.jpg",
+	"calibration\\mobil\\frames\\scaled\\0051.jpg",
+	"calibration\\mobil\\frames\\scaled\\0052.jpg",
+	"calibration\\mobil\\frames\\scaled\\0053.jpg",
+	"calibration\\mobil\\frames\\scaled\\0054.jpg",
+	"calibration\\mobil\\frames\\scaled\\0055.jpg",
+	"calibration\\mobil\\frames\\scaled\\0056.jpg",
+	"calibration\\mobil\\frames\\scaled\\0057.jpg",
+	"calibration\\mobil\\frames\\scaled\\0058.jpg",
+	"calibration\\mobil\\frames\\scaled\\0059.jpg",
+	"calibration\\mobil\\frames\\scaled\\test.jpg"
+	};
 
 int index = 0;
 
+#include <windows.h>
+
 int main()
 {
-	for (int i = 0; i < sizeof(names); i++)
+	char bf[1024];
+	GetModuleFileNameA(NULL, bf, 1024);
+	char * c = strrchr(bf, '\\') + 1;
+	(*c) = '\0';
+	std::string appPath = bf;
+
+	int total = sizeof(names) / sizeof(names[0]);
+	for (int i = 0; i < total; i++)
 	{
 		////opencv load image
-		cv::Mat image = cv::imread(names[i], CV_LOAD_IMAGE_COLOR);
+		
+		cv::Mat image = cv::imread( (appPath + names[i]).c_str(), CV_LOAD_IMAGE_COLOR);
 
 		//// move to grey
 		cv::Mat grey;
@@ -87,6 +97,12 @@ int main()
 		cv::Mat ret = image;
 		cv::Mat resized;
 		cv::Size size = image.size();
+
+		std::vector<cv::Point2f> crns;
+		bool chess = cv::findChessboardCorners(grey, cv::Size(7, 7), crns, 11);
+
+		if (chess)
+			__debugbreak();
 
 		double ratio1 = 800.0 / size.width;
 		double ratio2 = 600.0 / size.height;
@@ -125,10 +141,10 @@ int main()
 		cv::Mat result;
 		cv::resize(ret, result, size);
 
-		imshow("Result", result);
+		//imshow("Result", result);
 		// corresponding hugh line
 
-		cv::waitKey();
+		//cv::waitKey();
 	}
 	
 	return 0;
